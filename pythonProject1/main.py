0a6e5c67f7c83f87d1fab6b56c6472f70d586c0d
eylulgokce <44869816+eylulgokce@users.noreply.github.com>
2022-09-04 21:48:41 +0200

    Add files via upload

---
+++ pythonProject1/main.py
@@ -0,0 +1,28 @@
+import pathfinder as pf
+
+
+def create_graph():
+    return {
+        'A': {'B': 5, 'D': 1},
+        'B': {'A': 3, 'C': 1},
+        'C': {'A': 10, 'G': 4},
+        'D': {'E': 3},
+        'E': {'C': 2},
+        'F': {'A': 2, 'D': 7, 'G': 4},
+        'G': {'C': 1, 'E': 1, 'F': 1}
+    }
+
+
+if __name__ == '__main__':
+
+    graph = create_graph()
+    start_node = "G"
+    end_node = "B"
+
+    print("The path between", start_node, "to", end_node)
+
+    pf.bfs(graph, start_node, end_node)
+    dijkstra = pf.dijkstra(graph, start_node, end_node)
+
+
+
---
+++ pythonProject1/pathfinder.py
@@ -0,0 +1,82 @@
+def print_path(end_node, neighbour_node):
+    print("Following is the Dijkstra algorithm:")
+
+    path = ""
+    path = end_node + "->" + path
+    while True:
+        end_node = neighbour_node[end_node]
+        if end_node is None:
+            path += ""
+            break
+        path = end_node + "->" + path
+
+    print("Path (Dijsktra) =", path[:-2])
+    return path[:-2]
+
+
+def dijkstra(graph, start_node, end_node):
+    path = dict()
+    neighbour_node = dict()
+    unvisited_nodes = []
+
+    for node in graph:
+        path[node] = float("inf")  # lowest path cost
+        neighbour_node[node] = None
+        unvisited_nodes.append(node)
+
+    path[start_node] = 0
+
+    while unvisited_nodes:
+        key_min = unvisited_nodes[0]
+        min_val = path[key_min]
+        for i in range(1, len(unvisited_nodes)):
+            if path[unvisited_nodes[i]] < min_val:
+                key_min = unvisited_nodes[i]
+                min_val = path[key_min]
+        curr = key_min
+        unvisited_nodes.remove(curr)
+
+        for node in graph[curr]:
+            new_path = graph[curr][node] + path[curr]
+            if path[node] > new_path:
+                path[node] = new_path
+                neighbour_node[node] = curr
+
+    shortest_path = print_path(end_node, neighbour_node)
+    return shortest_path
+
+
+def bfs(graph, start_node, end_node):
+    visited = []
+    queue = [[start_node]]
+
+    if start_node == end_node:
+        print("Same Node")
+        return
+
+    while queue:
+        path = queue.pop(0)
+        node = path[-1]
+
+        # if current node is not visited
+        if node not in visited:
+            neighbours = graph[node]
+
+            # iterate over the neighbours of the node
+            for neighbour in neighbours:
+                new_path = list(path)
+                new_path.append(neighbour)
+                queue.append(new_path)
+
+                # nif neighbour node is destination
+                if neighbour == end_node:
+                    print("Following is the Breadth-First Search:")
+                    print("Path (BFS) = ", *new_path)
+                    return new_path
+            visited.append(node)
+
+    print("Connecting path doesn't exist!")
+    return
+
+
+
